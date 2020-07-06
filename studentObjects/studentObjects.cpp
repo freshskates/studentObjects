45740f2260d0a58f33add55e25d6d6aa2d9c9533
Robert <67438520+freshskates@users.noreply.github.com>
2020-07-06 12:04:05 -0700

    Add project files.

---
+++ studentObjects/studentObjects.cpp
@@ -0,0 +1,48 @@
+#include <iomanip> 
+#include <iostream> 
+using namespace std;
+
+struct Student
+{
+    string name;
+    string address;
+    string zip;
+
+
+
+
+
+};  
+
+Student check(Student& user, int number) {
+    cout << "Enter for Student " << number << endl;
+    cout << "Name: ";
+    cin >> user.name;
+    cin.ignore(1000, 10);
+    cout << "Adress: ";
+    cin >> user.address;
+    cin.ignore(1000, 10);
+    cout << "Zip: ";
+    cin >> user.zip;
+    cin.ignore(1000, 10);
+    return user;
+}
+void output(Student& user) {
+    cout << user.name << endl;
+    cout << user.address << endl;
+    cout << user.zip << endl;
+
+}
+int main()
+{
+    Student robert, joe, moma, cuh;
+    check(robert, 1);
+    check(joe, 2);
+    check(moma, 3);
+    check(cuh, 4);
+    output(robert);
+    output(joe);
+    output(moma);
+    output(cuh);
+
+}
